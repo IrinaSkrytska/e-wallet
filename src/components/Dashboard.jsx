f88479b5508c8470100984021b0ab160b3aac664
irinaskritska78 <irinaskritska77@gmail.com>
2025-05-06 12:44:54 +0300

    adds design fixes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -11,93 +11,82 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="dashboard-page">
-      {/* Navigation Bar */}
-      <nav className="navbar">
-        <div className="nav-left">
-          <span className="logo">BudgetWise</span>
-          <NavLink
-            to="/dashboard"
-            className={({ isActive }) =>
-              isActive ? "nav-link active" : "nav-link"
-            }
-          >
-            Dashboard
-          </NavLink>
-          <NavLink
-            to="/budgets"
-            className={({ isActive }) =>
-              isActive ? "nav-link active" : "nav-link"
-            }
-          >
-            Budgets
-          </NavLink>
-          <button className="add-budget">
-            <svg
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="addIcon"
-              viewBox="0 0 16 16"
+    <>
+      <div className="dashboard-thumb">
+        {/* Navigation Bar */}
+        <nav className="navbar">
+          <div className="nav-left">
+            <span className="logo">BudgetWise</span>
+            <NavLink
+              to="/dashboard"
+              className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+              }
             >
-              <path d="M8 7V1h1v6h6v1H9v6H8V8H2V7h6z" />
-            </svg>
-            Add a new budget
-          </button>
-        </div>
-        <div className="nav-right">
-          <button onClick={handleLogout} className="nav-link logout-btn">
-            Logout
-          </button>
-        </div>
-      </nav>
-      <div className="dashboard-content">
-        <div className="month-summary">
-          <div className="month-plan-thumb">
-            <p className="month-plan-text">PLAN</p>
+              Dashboard
+            </NavLink>
+            <NavLink
+              to="/budgets"
+              className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+              }
+            >
+              Budgets
+            </NavLink>
+            <button className="add-budget">
+              <svg
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="addIcon"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 7V1h1v6h6v1H9v6H8V8H2V7h6z" />
+              </svg>
+              Add a new budget
+            </button>
           </div>
-          <div className="month-summary-thumb">
-            <p className="month-summary-text">Current Month Summary</p>
+          <div className="nav-right">
+            <button onClick={handleLogout} className="nav-link logout-btn">
+              Logout
+            </button>
+          </div>
+        </nav>
 
-            <div className="summary-card">
-              <div className="summary-thumb">
-                <h4 className="summary-income-text">Income</h4>
-                <div className="income-progress-line">
-                  <div
-                    className="progress-dot"
-                    style={{
-                      position: "absolute",
-                      left: `32%`,
-                    }}
-                  ></div>
-                </div>
-                <div className="income-values-thumb">
-                  <p className="income-value">$500 earned</p>
-                  <p className="income-value">$4000 goal</p>
-                </div>
+        <div className="dashboard-page">
+          <div className="dashboard-content">
+            <div className="month-summary">
+              <div className="month-plan-thumb">
+                <p className="month-plan-text">Plan</p>
               </div>
+              <div className="month-summary-thumb">
+                <p className="month-summary-text shimmer">
+                  Current Month Summary
+                </p>
 
-              <div className="outcome-thumb">
-                <p className="summary-outcome-text">Total Outcome</p>
-                <div className="outcome-progress-line">
-                  <div
-                    className="outcome-progress-dot"
-                    style={{
-                      position: "absolute",
-                      left: `10%`,
-                    }}
-                  ></div>
-                </div>
-                <div className="outcome-values-thumb">
-                  <p className="outcome-value">$250 spent</p>
-                  <p className="outcome-value">$3000 budget</p>
+                <div className="summary-card">
+                  <div className="summary-thumb">
+                    <h4 className="summary-income-text">Income:</h4>
+
+                    <div className="income-values-thumb">
+                      <p className="income-value">$500 earned</p>
+                    </div>
+                  </div>
+
+                  <div className="outcome-thumb">
+                    <p className="summary-outcome-text">Outcome:</p>
+
+                    <div className="outcome-values-thumb">
+                      <p className="outcome-value">$250 spent</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
